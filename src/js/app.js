3e040c96583df8a7b72cf5ea8947c37693e6c1b9
kkid404 <vizzy1488@gmail.com>
2021-10-22 02:25:50 +0500

    Добавил анимацию бургера. Сделаю выезжающее меню. Верстаю по Perfect Pixel. Понадобился час.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
 import '../style/style.scss'
+
+document.querySelector('.burger__menu').addEventListener('click', function(){
+    document.querySelector('.burger__menu span').classList.toggle('active');
+  })
