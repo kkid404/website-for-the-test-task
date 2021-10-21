53c008a72200563f8ae13d8ab7082d25101ccd76
kkid404 <vizzy1488@gmail.com>
2021-10-22 03:31:16 +0500

    Убрал лишние свойства css. Переписал функцию на стрелочную. Добавил выезжающее адаптированное меню. Адаптировал шрифт. Понадобился час

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import '../style/style.scss'
 
-document.querySelector('.burger__menu').addEventListener('click', function(){
-    document.querySelector('.burger__menu span').classList.toggle('active');
+document.querySelector('.burger__menu').addEventListener('click', () =>{
+    document.querySelector('.burger__menu span').classList.toggle('active')
+    document.querySelector('.menu').classList.toggle('animate');
   })
